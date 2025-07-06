179095e28b843a4eac4f3fb6f6c5ce80bac860d6
Alexey Filichkin <filichkin.alexey@gmail.com>
2025-07-06 17:25:07 +0300

    upd PepParseItem

--- pep_parse/spiders/pep.py
+++ pep_parse/spiders/pep.py
@@ -2,9 +2,9 @@ import scrapy
 
 
 class PepSpider(scrapy.Spider):
-    name = "pep"
-    allowed_domains = ["peps.python.org"]
-    start_urls = ["https://peps.python.org"]
+    name = 'pep'
+    allowed_domains = ['peps.python.org']
+    start_urls = ['https://peps.python.org']
 
     def parse(self, response):
         pass
